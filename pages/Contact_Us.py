3e73d0912577805e35ffd497007475f4bc5c1bce
Emmanuel <emmanueljoeemie@gmail.com>
2024-07-23 23:53:01 +0100

    Day22:send email to self

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 
-st
+with st.form(key='myemailForm'):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Your message here")
+    button = st.form_submit_button("Submit")
