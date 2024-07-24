4605d6e2fc6dba963b004517d0029abb246d7ad3
Emmanuel <emmanueljoeemie@gmail.com>
2024-07-24 23:18:16 +0100

    .....

--- Home.py
+++ Home.py
@@ -10,9 +10,14 @@ with col1:
 
 with col2:
     st.title("EMMANUEL PATRICK")
-    content = """Hi..I am Emmanuel,a passionate Python developer with a year of hands-on experience in building robust and scalable applications. With a background in computer science and a keen interest in problem-solving, John has honed his skills in Python development through various projects and continuous learning.
+    content = """Hi..I am Emmanuel,a passionate Python developer with a year of hands-on experience in building robust 
+    and scalable applications. With a background in computer science and a keen interest in problem-solving, 
+    Emmanuel has honed his skills in Python development through various projects and continuous learning.
 
-Over the past year, Emmanuel has worked on diverse projects ranging from web development to data analysis. He has a solid understanding of Python libraries such as Django, Flask, Pandas, and NumPy, which he has utilized to create efficient web applications and perform complex data manipulations.
+Over the past year, Emmanuel has worked on diverse projects ranging from web development to data analysis. He has a
+ solid 
+understanding of Python libraries such as Django, Flask, Pandas, and NumPy, 
+which he has utilized to create efficient web applications and perform complex data manipulations.
 
     """
 
@@ -22,14 +27,14 @@ content2 = """
 Below you can find some of the apps I have built in python. Feel free to contact me"""
 st.write(content2)
 
-col3,empty_col, col4 = st.columns([1.5,0.5,1.5])
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
         st.write(row["description"])
-        st.image('images/'+ row['image'])
+        st.image('images/' + row['image'])
         st.write(f"[Source Code]({row['url']})")
 
 with col4:
--- sent_email.py
+++ sent_email.py
@@ -1,21 +1,21 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "emmanueljoeemie@gmail.com"
-    password = "sbpzhwriqaqhegyb"
+    password = os.getenv('GMAIL_PASSWORD')
     receiver = "emmanueljoeemie@gmail.com"
     context = ssl.create_default_context()
-#     message = """\
-# Subject: Hi!
-# How are you?
-# Bye!
-# """
+    #     message = """\
+    # Subject: Hi!
+    # How are you?
+    # Bye!
+    # """
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
-
 
 # send_email()
